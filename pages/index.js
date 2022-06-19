bf6b0cf67091f8ccc228da62c8bacdd6df9fc688
Jordan Ambra <jordan@serenity.software>
2022-06-19 14:01:06 -0400

    Replace hardcoded data with a MongoDB lookup

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,17 @@
 import Profile from '../components/profile';
+import { getDateSlug } from '../lib/dates';
+import clientPromise from '../lib/mongodb';
 
 export async function getServerSideProps(context) {
+  const date = getDateSlug();
+  const client = await clientPromise;
+  const db = client.db("aotd");
+  const app = await db.collection("apps").findOne({date: date});
+  delete app._id;
+
   return {
     props: {
-      app: {
-        title: 'Clara',
-        subtitle: 'Human-in-the-loop assistant that schedules meetings.',
-        description: 'Clara helps you focus on things that matter. Just add Clara to any email, and it will take care of scheduling back-and-forth on your behalf.',
-        creator: 'ClaraLabs',
-        pricing: {
-          trial: true,
-          currency: '$',
-          minimum: 99,
-          maximum: 399,
-          period: 'month',
-          model: 'per user'
-        },
-        tags: ['Productivity', 'Calendar'],
-        images: {
-          main: 'https://ph-files.imgix.net/2e458295-b27b-4471-a3f4-b0c27ee6362d.png?auto=format&auto=compress&codec=mozjpeg&cs=strip',
-          icon: 'https://ph-files.imgix.net/4acd998d-9139-4a71-b278-d76df8382665.gif?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=64&h=64&fit=crop&frame=1&dpr=1',
-        },
-        date: '2022-06-18',
-        slug: 'clara'
-      }
+      app
     }
   }
 }
